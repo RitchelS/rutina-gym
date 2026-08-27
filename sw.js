c2aa9414b81be643a4b75447f50baa2e4e1c1c7b
RitchelS <77294614+RitchelS@users.noreply.github.com>
2026-08-27 10:38:46 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-rutina-v2';
+const CACHE = 'mi-rutina-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
